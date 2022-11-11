4be6ab54c38a9996c9c8c51ce4625fbffacf1d2a
GigiDev7 <gigivadachkoria@gmail.com>
2022-11-11 13:05:27 +0400

    added some reusable components,styling

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,27 +1,52 @@
 <template>
   <section>Filter</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
-    </div>
-    <ul>
-      <li v-for="coach in filteredCoaches" :key="coach.id">
-        {{ coach.firstName }}
-      </li>
-    </ul>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register as Coach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :coach="coach"
+          :key="coach.id"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No coaches found.</h3>
+    </base-card>
   </section>
 </template>
 
 <script>
 //import { mapGetters } from 'vuex';
+import CoachItem from '../../components/coaches/CoachItem.vue';
 
 export default {
+  components: {
+    CoachItem,
+  },
   computed: {
     //...mapGetters('coaches', ['coaches']),
     filteredCoaches() {
       return this.$store.getters['coaches/coaches'];
     },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
   },
 };
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
